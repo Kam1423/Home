e68bf4a7c18d08ac4580f58dcad9758ccf615887
Bas <632021101@tsu.ac.th>
2024-02-16 09:18:29 +0700

    Validate

---
+++ src/constants/appointmenu.js
@@ -0,0 +1,28 @@
+﻿import {Platform} from 'react-native';
+export default [
+  {
+    title: 'จองคิววันนี้',
+    image: '../images/feature-appointment.jpg',
+    source: require("../images/feature-appointment.jpg"),
+        navigate: Platform.OS === 'android' ? 'AppointUploadAndroid' : 'AppointUpload' 
+  },
+  {
+      title: 'นัดล่วงหน้า',
+      source: require("../images/feature-service.jpg"),
+      price: 40,
+      navigate: 'Appoint2'
+  },
+  {
+      title: 'คลินิกทั่วไป',
+      source: require("../images/feature-service.jpg"),
+      price: 40,
+      navigate: 'AppointCalendar'
+  },
+  {
+      title: 'คลินิกพิเศษเฉพาะทางนอกเวลาราชการ (SMC)',
+      source: require("../images/feature-service.jpg"),
+      price: 40,
+      navigate: 'AppointCalendar'
+  }
+
+];
---
+++ src/screens/Signup.js
@@ -0,0 +1,304 @@
+import React, {useState} from "react";
+import { StatusBar } from 'expo-status-bar';
+import { View, TouchableOpacity, Text } from "react-native";
+
+// Formik
+import { Formik } from "formik";
+// Icons
+import { Octicons } from '@expo/vector-icons';
+// Datetimepicker
+import DateTimePicker from '@react-native-community/datetimepicker';
+// Keyboard avoiding view
+import KeyboardAvoidingWrapper from "./../components/KeyboardAvoidingWrapper";
+
+import {
+    StyledContainer,
+    InnerContainer,
+    SubTitle,
+    StyledFormArea,
+    LeftIcon,
+    StyledInputLabel,
+    StyledButton,
+    ButtonText,
+    Colors,
+    PageLogo,
+} from "../constants/styles";
+
+import { TextInput } from "react-native-gesture-handler";
+
+// Colors
+const { LightGray} = Colors;
+
+const Signup = ({navigation}) => {
+    const [show, setShow] = useState(false);
+    const [date, setDate] = useState(new Date(2023, 0, 1));
+    
+    // Actual Date of Birth
+    // Actual Date of Birth
+    const [dob, setDob] = useState(new Date());
+    
+
+
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setShow(false);
+        setDate(currentDate);
+        setDob(currentDate); // เปลี่ยนจาก setDob(currentDate) เป็น setDob(new Date(currentDate))
+    }
+
+    const showDatePicker = () => {
+        setShow(true);
+    }
+
+
+    return (
+        <KeyboardAvoidingWrapper>
+        <StyledContainer>
+          <StatusBar style="dark" />
+            <InnerContainer>
+                <PageLogo resizeMode="cover" source={require('../../assets/images/skinlogo.jpg')} /> 
+                <SubTitle> สมัครสมาชิก </SubTitle>
+                
+        {show && (
+            <DateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            mode='date'
+            is24Hour={true}
+            display="default" // เปลี่ยนจาก "default" เป็น "spinner"
+            onChange={onChange}
+        />
+)}
+
+
+                <Formik
+                    initialValues={{
+                                    firstname: '',
+                                    lastname: '',
+                                    id_card: '',
+                                    phone: '',
+                                    dateofBirth: '',
+                                }}
+                                validate={(values) => {
+                                    const errors = {};
+                            
+                                    if (!values.firstname.trim()) {
+                                        errors.firstname = 'กรุณากรอกชื่อ';
+                                    }
+                            
+                                    if (!values.lastname.trim()) {
+                                        errors.lastname = 'กรุณากรอกนามสกุล';
+                                    }
+                            
+                                    if (!values.id_card.trim()) {
+                                        errors.id_card = 'กรุณากรอกเลขบัตรประชาชน';
+                                    } else if (values.id_card.trim().length !== 13) {
+                                        errors.id_card = 'เลขบัตรประชาชนต้องมี 13 หลัก';
+                                    }
+                            
+                                    if (!values.phone.trim()) {
+                                        errors.phone = 'กรุณากรอกเบอร์โทรศัพท์';
+                                    } else if (values.phone.trim().length !== 10) {
+                                        errors.phone = 'เบอร์โทรศัพท์ต้องมี 10 หลัก';
+                                    }
+                            
+                                    if (!values.dateofBirth.trim()) {
+                                        errors.dateofBirth = 'กรุณาเลือกวันเกิด';
+                                    }
+                            
+                                    return errors;
+                                }}
+                                onSubmit={(values) => {
+                                    console.log(values);
+                                    navigation.navigate('DeviceScene');
+                                }}
+                            >
+
+                {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+
+                    <StyledFormArea>
+                        {/* Text input ของ ชื่อ */}
+                        <WordTextInput 
+                            label="ชื่อ"
+                            icon="person"
+                            value={values.firstname}
+                            onChangeText={handleChange('firstname')}
+                            onBlur={handleBlur('firstname')}
+                            error={errors.firstname}
+                        />
+
+                        {/* Text input ของ นามสกุล */}
+                        <WordTextInput 
+                            label="นามสกุล"
+                            icon="person"
+                            value={values.lastname}
+                            onChangeText={handleChange('lastname')}
+                            onBlur={handleBlur('lastname')}
+                            error={errors.lastname}
+                        />
+
+                        {/* Text input ของ บัตรประชาชน */}
+                        <NumberIDTextInput
+                            label="บัตรประชาชน"
+                            icon="credit-card"
+                            value={values.id_card}
+                            onChangeText={handleChange('id_card')}
+                            onBlur={handleBlur('id_card')}
+                            error={errors.id_card}
+                        />
+
+                        {/* Text input ของ เบอร์โทรศัพท์ */}
+                        <NumberTextInput 
+                            label="เบอร์โทรศัพท์"
+                            icon="megaphone"
+                            value={values.phone}
+                            onChangeText={handleChange('phone')}
+                            onBlur={handleBlur('phone')}
+                            error={errors.phone}
+                        />
+
+                        {/* Text input ของ วันเดือนปีเกิด */}
+                        <DateTextInput 
+                            label="วัน/เดือน/ปี เกิด"
+                            icon="calendar"
+                            value={dob instanceof Date ? dob.toDateString() : ''}
+                            onChangeText={handleChange('dateofBirth')}
+                            onBlur={handleBlur('dateofBirth')}
+                            showDatePicker={showDatePicker}
+                            error={errors.dateofBirth}
+                        />
+
+                        <StyledButton onPress={handleSubmit}>
+                            <ButtonText>สมัครสมาชิก</ButtonText>
+                        </StyledButton>
+                    </StyledFormArea>
+                 )}
+            </Formik>
+                </InnerContainer>
+        </StyledContainer>
+        </KeyboardAvoidingWrapper>
+    );
+};
+
+// Text input ใช้ Name
+const WordTextInput = ({icon, label, value, onChangeText, error})=>{
+    return(
+        <View>
+            <LeftIcon>
+            <Octicons name={icon} size={20} color="black" />
+            </LeftIcon>
+            <StyledInputLabel>{label}</StyledInputLabel>
+                <TextInput 
+                    value={value}
+                    placeholder="กรุณากรอกข้อมูล"
+                    onChangeText={onChangeText}
+                    style={{
+                        paddingRight:40,
+                        paddingLeft:40,
+                        fontSize:18,
+                        height:50,
+                        marginVertical:3,
+                        marginBottom:15,
+                        backgroundColor:LightGray,
+                        opacity:0.5,
+                    }}
+                    keyboardType="email-address"
+                />
+                {error && <Text style={{color: 'red'}}>{error}</Text>}
+        </View>
+    )
+}
+
+// Text input ใช้ Number
+const DateTextInput = ({ icon, label, value, onChangeText, showDatePicker, error }) => {
+    const formattedValue = value instanceof Date ? value.toDateString() : '';
+  
+    return (
+      <View>
+        <LeftIcon>
+          <Octicons name={icon} size={20} color="black" />
+        </LeftIcon>
+        <StyledInputLabel>{label}</StyledInputLabel>
+        <TouchableOpacity onPress={showDatePicker}>
+          <TextInput
+            value={formattedValue}
+            placeholder="วัน เดือน ปี"
+            onChangeText={onChangeText}
+            style={{
+              paddingRight: 40,
+              paddingLeft: 40,
+              fontSize: 18,
+              height: 50,
+              marginVertical: 3,
+              marginBottom: 15,
+              backgroundColor: LightGray,
+              opacity: 0.5,
+            }}
+            editable={false}
+          />
+        </TouchableOpacity>
+        {error && <Text style={{ color: 'red' }}>{error}</Text>}
+      </View>
+    );
+  };
+  
+
+// Text input ใช้ กรอกเบอร์โทรศัพท์
+const NumberTextInput = ({icon, label, value, onChangeText, error})=>{
+    return(
+        <View>
+            <LeftIcon>
+            <Octicons name={icon} size={20} color="black"/>
+            </LeftIcon>
+            <StyledInputLabel>{label}</StyledInputLabel>
+                <TextInput 
+                    value={value}
+                    placeholder="กรอกเบอร์โทรศัพท์"
+                    onChangeText={onChangeText}
+                    style={{
+                        paddingRight:40,
+                        paddingLeft:40,
+                        fontSize:18,
+                        height:50,
+                        marginVertical:3,
+                        marginBottom:15,
+                        backgroundColor:LightGray,
+                        opacity:0.5,
+                    }}
+                    keyboardType="number-pad"
+                    maxLength={10}
+                />
+                {error && <Text style={{color: 'red'}}>{error}</Text>}
+        </View>
+    )
+}
+// Text input ใช้ กรอกเลขบัตรประชาชน
+const NumberIDTextInput = ({icon, label, value, onChangeText, error})=>{
+    return(
+        <View>
+            <LeftIcon>
+            <Octicons name={icon} size={20} color="black"/>
+            </LeftIcon>
+            <StyledInputLabel>{label}</StyledInputLabel>
+                <TextInput 
+                    value={value}
+                    placeholder="กรอกเลขบัตรประชาชน"
+                    onChangeText={onChangeText}
+                    style={{
+                        paddingRight:40,
+                        paddingLeft:40,
+                        fontSize:18,
+                        height:50,
+                        marginVertical:3,
+                        marginBottom:15,
+                        backgroundColor:LightGray,
+                        opacity:0.5,
+                    }}
+                    keyboardType="number-pad"
+                    maxLength={13}
+                />
+                {error && <Text style={{color: 'red'}}>{error}</Text>}
+        </View>
+    )
+}
+export default Signup;
